3eab2751919b383266f81b7d7bbe9661901a6603
dilrong <wet932@naver.com>
2020-04-09 15:39:01 +0900

    Init: FileArchitecture

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  env: {
+    browser: true,
+    jest: true,
+    es6: true
+  },
+  extends: [
+    'standard'
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly'
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module'
+  },
+  plugins: [
+    '@typescript-eslint'
+  ],
+  rules: {
+  }
+}
---
+++ src/controllers/api.ts
@@ -0,0 +1,10 @@
+import express, { Request, Response } from 'express'
+import { Container } from 'typedi'
+
+const router = express.Router()
+
+router.get('/', async (req: Request, res: Response) => {
+  
+})
+
+export default router
